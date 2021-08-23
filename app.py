435784a3aff5e3f2bbda7e7c5ecdb4a0450d0890
Nguyen Duong Minh Tam Dat <nguyentamdat@gmail.com>
2021-08-23 10:36:30 +0700

    Wrapper (#4)
    * add accent
    
    * add requirement
    
    Co-authored-by: ngoc.ho31 <phuongngoc31@gmail.com>

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+from tensorflow.python.framework.tensor_conversion_registry import get
 import uvicorn
 from fastapi import FastAPI
 from correct_teencode import correct_teencode
 from correct_telex import TelexErrorCorrector
 from pydantic import BaseModel
+from accent import get_accented
 
 telexCorrector = TelexErrorCorrector()
 
@@ -26,6 +28,11 @@ def telex(data: Request):
     corrected = telexCorrector.fix_telex_sentence(data["text"])
     return {"result": corrected}
 
+@app.post("/correct-accent")
+def accent(data: Request):
+    data = data.dict()
+    corrected = get_accented(data["text"])
+    return {"result": corrected}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000)
