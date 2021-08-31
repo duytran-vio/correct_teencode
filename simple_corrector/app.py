f266ec89caac08bc7452e631675d5c7ebb2b33aa
ngoc.ho31 <phuongngoc31@gmail.com>
2021-08-31 11:30:26 +0700

    update organization

--- simple_corrector/app.py
+++ simple_corrector/app.py
@@ -1,12 +1,14 @@
-from tensorflow.python.framework.tensor_conversion_registry import get
 import uvicorn
 from fastapi import FastAPI
 from correct_teencode import correct_teencode
 from correct_telex import TelexErrorCorrector
 from pydantic import BaseModel
 from accent import get_accented
+from correct_close_character import correct_close_character_sent
+import requests
+import os
 
-telexCorrector = TelexErrorCorrector()
+
 
 app = FastAPI()
 
@@ -28,11 +30,22 @@ def telex(data: Request):
     corrected = telexCorrector.fix_telex_sentence(data["text"])
     return {"result": corrected}
 
+
+@app.post("/correct-close")
+def close(data: Request):
+    data = data.dict()
+    corrected = correct_close_character_sent(data["text"])
+    return {"result": corrected}
+
+
 @app.post("/correct-accent")
 def accent(data: Request):
     data = data.dict()
-    corrected = get_accented(data["text"])
+    response = requests.post(os.environ["ACCENT_URL"], json={"text": data["text"]}).json()
+    corrected = response["result"]
     return {"result": corrected}
 
+
 if __name__ == "__main__":
+    telexCorrector = TelexErrorCorrector()
     uvicorn.run(app, host="0.0.0.0", port=5000)
---
+++ simple_corrector/app.sh
@@ -0,0 +1,2 @@
+python app.py 'vn_accent/data/tokenizer.h5' 'vn_accent/experiments/transformer_evolved/weights/epoch_01.h5' \
+--model_name 'transformer_evolved'
