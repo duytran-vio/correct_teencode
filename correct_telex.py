c2fcb4d3bfcbec604c6052c14f64772df1178232
Duy Tran <ducduyvio@gmail.com>
2021-09-10 17:46:32 +0700

    update english word for correct_telex

--- correct_telex.py
+++ correct_telex.py
@@ -16,6 +16,8 @@ class TelexErrorCorrector:
     '''
     fi = open('telex/complex_telex_fault.json', encoding='utf-8')
     complex_telex = json.load(fi)
+    eng_file = open('teencode/eng_dic.json', 'r')
+    eng_dic = json.load(eng_file)
 
     def __init__(self):
         self.build_character_regexs()
@@ -27,40 +29,42 @@ class TelexErrorCorrector:
         return ' '.join(words)
 
     def fix_telex_word(self, word):
+        try:
+            if self.eng_dic[word] == 1: return word
+        except:
+            ## covert accents to keystrokes
+            word = word.lower()
+            word = unicodedata.normalize('NFKD', word)
+            for accent, keystroke in self.accent_to_telex.items():
+                word = re.sub(accent, keystroke, word)
 
-        ## covert accents to keystrokes
-        word = word.lower()
-        word = unicodedata.normalize('NFKD', word)
-        for accent, keystroke in self.accent_to_telex.items():
-            word = re.sub(accent, keystroke, word)
+            ## push keystrokes to the end of word
+            reorder_word = word
+            i = 1
+            n = len(word)
+            while (i < n):
+                a = reorder_word[i]
+                if a in self.additional_keystrokes + ['w'] and (reorder_word[i - 1] + a != 'tr'):
+                    reorder_word = reorder_word[:i] + reorder_word[i+1:] + a
+                    n = n - 1
+                else:
+                    i = i + 1
+            word = reorder_word
 
-        ## push keystrokes to the end of word
-        reorder_word = word
-        i = 1
-        n = len(word)
-        while (i < n):
-            a = reorder_word[i]
-            if a in self.additional_keystrokes + ['w'] and (reorder_word[i - 1] + a != 'tr'):
-                reorder_word = reorder_word[:i] + reorder_word[i+1:] + a
-                n = n - 1
-            else:
-                i = i + 1
-        word = reorder_word
+            word = unicodedata.normalize('NFC', word)
 
-        word = unicodedata.normalize('NFC', word)
+            for key, value in self.char_telex_errors.items():
+                word = re.sub(key, value, word)
 
-        for key, value in self.char_telex_errors.items():
-            word = re.sub(key, value, word)
+            ## 'trưong' -> 'trương'
+            word = re.sub('ưo', 'ươ', word)
 
-        ## 'trưong' -> 'trương'
-        word = re.sub('ưo', 'ươ', word)
+            for key, value in self.accent_telex_errors.items():
+                word = re.sub(key, value, word)
 
-        for key, value in self.accent_telex_errors.items():
-            word = re.sub(key, value, word)
-
-        for key, value in self.complex_telex.items():
-            word = re.sub(key, value, word) 
-        return word
+            for key, value in self.complex_telex.items():
+                word = re.sub(key, value, word) 
+            return word
 
     def build_character_regexs(self):
         chars = ['ư', 'â', 'ă', 'ô', 'ơ', 'ê']
@@ -109,6 +113,6 @@ class TelexErrorCorrector:
 # %%
 if __name__ == "__main__":
     corrector = TelexErrorCorrector()
-    fixed = corrector.fix_telex_sentence('huywejn')
+    fixed = corrector.fix_telex_sentence('set')
     print(fixed)
 # %%
